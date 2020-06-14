a83a57b5bee7b50d713ac9bf69ac73a2b4fda0d6
Dhosio <danielgithiomi@gmail.com>
2020-06-14 20:01:55 +0300

    Completed the network calls for the categories

--- app/src/main/java/com/githiomi/renu/models/Constants.java
+++ app/src/main/java/com/githiomi/renu/models/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
 //    For the base url
     // main
-
+    public static final String BASE_URL = "https://dhosio-renudatabase.herokuapp.com/";
     //routes
     public static final String CATEGORY_ROUTE = "categories";
 }
--- app/src/main/java/com/githiomi/renu/network/RenuClient.java
+++ app/src/main/java/com/githiomi/renu/network/RenuClient.java
@@ -1,4 +1,43 @@
 package com.githiomi.renu.network;
 
+import com.githiomi.renu.models.Constants;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RenuClient {
+
+    public static Retrofit retrofit = null;
+
+    public static synchronized RenuApi getRenuApi(){
+
+        if ( retrofit == null ){
+
+            Interceptor interceptor = new Interceptor() {
+                @NotNull
+                @Override
+                public Response intercept(@NotNull Chain chain) throws IOException {
+                    Request request = chain.request().newBuilder().build();
+                    return chain.proceed(request);
+                }
+            };
+
+            OkHttpClient.Builder okhttp3Client = new OkHttpClient.Builder().addInterceptor(interceptor);
+
+            retrofit = new Retrofit.Builder()
+                        .baseUrl(Constants.BASE_URL)
+                        .client(okhttp3Client.build())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+        }
+        return retrofit.create(RenuApi.class);
+    }
 }
