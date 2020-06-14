b02dda4b885caa1b0e09678d8f4372ec24aec66a
Dhosio <danielgithiomi@gmail.com>
2020-06-14 19:54:03 +0300

    Added the constants class

---
+++ app/src/main/java/com/githiomi/renu/models/Constants.java
@@ -0,0 +1,10 @@
+package com.githiomi.renu.models;
+
+public class Constants {
+
+//    For the base url
+    // main
+
+    //routes
+    public static final String CATEGORY_ROUTE = "categories";
+}
--- app/src/main/java/com/githiomi/renu/network/RenuApi.java
+++ app/src/main/java/com/githiomi/renu/network/RenuApi.java
@@ -1,4 +1,16 @@
 package com.githiomi.renu.network;
 
+import com.githiomi.renu.models.Category;
+import com.githiomi.renu.models.Constants;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RenuApi {
+
+    @GET(Constants.CATEGORY_ROUTE)
+        Call< List<Category> > getCategories();
+
 }
