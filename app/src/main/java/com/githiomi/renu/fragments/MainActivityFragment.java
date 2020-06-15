276620991f24e3db8cce0fe99c169b156e8b6a50
Dhosio <danielgithiomi@gmail.com>
2020-06-15 09:50:30 +0300

    Altered the color resource file to fit my colors

---
+++ app/src/main/java/com/githiomi/renu/fragments/MainActivityFragment.java
@@ -0,0 +1,121 @@
+package com.githiomi.renu.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.githiomi.renu.R;
+import com.githiomi.renu.models.Category;
+import com.githiomi.renu.network.RenuApi;
+import com.githiomi.renu.network.RenuClient;
+import com.githiomi.renu.ui.MainActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivityFragment extends Fragment {
+
+//    TAG
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
+
+//    Local variables
+    //for all the categories
+    private List<Category> mAllCategories;
+    // List of category names
+    private List<String> mCategoryNames;
+
+//    Binding widgets
+    @BindView(R.id.tvIdentifier) TextView mOnlyTextView;
+
+    public MainActivityFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainActivityFragment newInstance() {
+        MainActivityFragment fragment = new MainActivityFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// Inflate the layout for this fragment
+        View mainView = inflater.inflate(R.layout.fragment_main_activity, container, false);
+
+//        Butter knife
+        ButterKnife.bind(this, mainView);
+
+//        This is the method call that will be used to get data from my API
+        getMenuCategories();
+
+        return mainView;
+    }
+
+
+    //    Implementation of the method that will get data from my API
+    private void getMenuCategories() {
+        Log.d(TAG, "getMenuCategories: method init");
+
+        RenuApi renuApi = RenuClient.getRenuApi();
+
+        Call<List<Category>> call = renuApi.getCategories();
+
+        call.enqueue(new Callback< List<Category> >() {
+            @Override
+            public void onResponse(Call< List<Category> > call, Response< List<Category> > response) {
+
+                if ( response.isSuccessful() ) {
+                    Log.d(TAG, "onResponse: Call was successful and response was gotten");
+
+                    mAllCategories = new ArrayList<>();
+                    mCategoryNames = new ArrayList<>();
+
+                    mAllCategories = response.body();
+
+                    for ( int i  = 0; i < mAllCategories.size(); i += 1 ){
+
+                        String categoryName = mAllCategories.get(i).getName();
+                        mCategoryNames.add(categoryName);
+
+                    }
+                    Log.d(TAG, "onResponse: These are all the category names: " + mCategoryNames);
+                    mOnlyTextView.setText("Success");
+                }
+                else {
+                    Log.d(TAG, "onResponse: Call was made but responce was not successful");
+                    mOnlyTextView.setText("Error");
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Category>> call, Throwable t) {
+                Log.d(TAG, "onFailure: Call was not able to be made");
+
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/githiomi/renu/ui/MainActivity.java
+++ app/src/main/java/com/githiomi/renu/ui/MainActivity.java
@@ -3,14 +3,30 @@ package com.githiomi.renu.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.githiomi.renu.R;
+import com.githiomi.renu.models.Category;
+import com.githiomi.renu.network.RenuApi;
+import com.githiomi.renu.network.RenuClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+//    TAG
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: MainActivity init");
     }
 }
