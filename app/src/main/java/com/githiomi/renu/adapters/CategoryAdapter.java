6bcf7026fb344108a9df6d5ad931835692c7babb
Dhosio <danielgithiomi@gmail.com>
2020-06-15 13:17:02 +0300

    Created the category adapter with no logic inside it

---
+++ app/src/main/java/com/githiomi/renu/adapters/CategoryAdapter.java
@@ -0,0 +1,33 @@
+package com.githiomi.renu.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class CategoryViewHolder extends RecyclerView.ViewHolder {
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
