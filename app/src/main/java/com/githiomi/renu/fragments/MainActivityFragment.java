0d034d3dd3a0331b9f2d6766b382bb1bdf92f5fe
Dhosio <danielgithiomi@gmail.com>
2020-06-15 15:02:27 +0300

    Implemented the progress abr to show while loading

--- app/src/main/java/com/githiomi/renu/fragments/MainActivityFragment.java
+++ app/src/main/java/com/githiomi/renu/fragments/MainActivityFragment.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.githiomi.renu.R;
@@ -46,6 +47,8 @@ public class MainActivityFragment extends Fragment {
 
 //    Binding widgets
     @BindView(R.id.recyclerCategory) RecyclerView mRecyclerCategoryView;
+    @BindView(R.id.prLoadingCategories) ProgressBar mProgressBar;
+    @BindView(R.id.tvErrorMessage) TextView mErrorMessage;
 
     public MainActivityFragment() {
         // Required empty public constructor
@@ -75,14 +78,16 @@ public class MainActivityFragment extends Fragment {
 //        Butter knife
         ButterKnife.bind(this, mainView);
 
+//        Calling the progress abr to start loading when the fragment is created
+        showLoading();
+
 //        This is the method call that will be used to get data from my API
         getMenuCategories();
 
         return mainView;
     }
 
-
-    //    Implementation of the method that will get data from my API
+//    Implementation of the method that will get data from my API
     private void getMenuCategories() {
         Log.d(TAG, "getMenuCategories: method init");
 
@@ -97,17 +102,14 @@ public class MainActivityFragment extends Fragment {
                 if ( response.isSuccessful() ) {
                     Log.d(TAG, "onResponse: Call was successful and response was gotten");
 
+                    // Method to show recycler view because response != null
+                    showIsSuccess();
+
                     mAllCategories = new ArrayList<>();
                     mCategoryNames = new ArrayList<>();
 
                     mAllCategories = response.body();
 
-                    for ( int i  = 0; i < mAllCategories.size(); i += 1 ){
-
-                        String categoryName = mAllCategories.get(i).getName();
-                        mCategoryNames.add(categoryName);
-
-                    }
                     Log.d(TAG, "onResponse: These are all the category names: " + mCategoryNames);
 
                     passToAdapter(mAllCategories);
@@ -121,7 +123,7 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onFailure(Call<List<Category>> call, Throwable t) {
                 Log.d(TAG, "onFailure: Call was not able to be made");
-
+                showIsFailure();
             }
         });
 
@@ -137,5 +139,21 @@ public class MainActivityFragment extends Fragment {
         mRecyclerCategoryView.setLayoutManager(gridLayoutManager);
 
         categoryAdapter.notifyDataSetChanged();
+    }
+
+//    These are the methods that are used to control the layout depending on the availability of output
+    private void showLoading() {
+        mProgressBar.setVisibility(View.VISIBLE);
+    }
+
+    private void showIsSuccess() {
+        mProgressBar.setVisibility(View.GONE);
+        mRecyclerCategoryView.setVisibility(View.VISIBLE);
+        Log.d(TAG, "showIsSuccess: output was retrieved");
+    }
+
+    private void showIsFailure() {
+        mProgressBar.setVisibility(View.GONE);
+        mErrorMessage.setVisibility(View.VISIBLE);
     }
 }
